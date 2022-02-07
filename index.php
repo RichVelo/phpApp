cb81baacb84fe00dc341db3019bb035aa06a93c9
Rich <richrar@gmail.com>
2022-02-07 13:56:45 +0000

    adding SQL link to index and functions.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+
+$db = new PDO('mysql:host=db; dbname=rbicycles', 'root', 'password');
+
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare('SELECT `bikeName`,`bikeClass`, `condition` FROM `bikes`');
+
+$query->execute();
+
+$allBikes = $query->fetchAll();
+
+echo '<h1>My whips</h1>';
+
+echo '<ul>';
+foreach($allBikes as $bikes) {
+    echo '<li>' . $bikes['bikeName'] . ' is a ' . $bikes['bikeClass'] . ' bike ' . ' and it\'s condition is ' . $bikes['condition'] . '</li>';
+}
+echo '</ul>';
