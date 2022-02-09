2c5c50b6f87d7954220e147fbc7aa82909df790b
Rich <richrar@gmail.com>
2022-02-09 14:57:10 +0000

    changes

--- functions.php
+++ functions.php
@@ -3,11 +3,12 @@
 function bikeDetails(array $allBikes): string
 {
     if (count($allBikes) == count($allBikes, COUNT_RECURSIVE)) {
-        return 'No information held within the array or cannot access it';
+        //checking that there is an array within an array to play with and then if not:
+        return 'There is a problem with the array - it should be an array within an array which provides the information from the db';
     }
-        $HTMLBikeCard = "";
+        $HTMLToDisplayBikeInfo = "";
         foreach ($allBikes as $bike) {
-            $HTMLBikeCard .= '<div class="bikeCard"><ul><il class="bike"><h2>Name:</h2><p>'. $bike['bikeName'] . '</p><h3>Brand and model:</h3><p>' . $bike['brand'] . ' ' . $bike['model'] . '</p><h3>Bike Type:</h3><p>' . $bike['bikeClass'] . '</p><h3>Condition:</h3><p>' . $bike['condition'] . '</p><h3>Colour:</h3><p>' . $bike['colour'] . '</p></il></ul></div>';
+            $HTMLToDisplayBikeInfo .= '<div class="bikeCard"><h2>Name:</h2><p>'. $bike['bikeName'] . '</p><h3>Brand and model:</h3><p>' . $bike['brand'] . ' ' . $bike['model'] . '</p><h3>Bike Type:</h3><p>' . $bike['bikeClass'] . '</p><h3>Condition:</h3><p>' . $bike['condition'] . '</p><h3>Colour:</h3><p>' . $bike['colour'] . '</p></div>';
         }
-        return $HTMLBikeCard;
+        return $HTMLToDisplayBikeInfo;
 }
