56868e34325f12d4eb451e34983eed1a6a566439
Rich <richrar@gmail.com>
2022-02-09 15:49:31 +0000

    changes as requested

--- index.php
+++ index.php
@@ -9,7 +9,13 @@ $allBikes = $query->fetchAll();
 <!DOCTYPE html>
 <html lang="en-GB">
 <head>
+    <link rel="stylesheet" type="text/css" href="normalize.css">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500;700;800&display=swap" rel="stylesheet">
     <title>Richie's Whips</title>
 </head>
 <body>
@@ -18,7 +24,7 @@ $allBikes = $query->fetchAll();
     </header>
     <main>
         <section class="bikeContainer">
-            <?= bikeDetails($allBikes)?>;
+            <?= bikeDetails($allBikes)?>
         </section>
     </main>
 </body>
