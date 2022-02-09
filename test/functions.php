bc91bb05bad6848857d26a1a53fbc9d71dc50b86
Rich <richrar@gmail.com>
2022-02-09 16:24:16 +0000

    test functions change to match the actual function

--- test/functions.php
+++ test/functions.php
@@ -8,7 +8,7 @@ class Functions extends TestCase
     public function testSuccessBikeDetails()
     {
         $input = [ ['id' => 1, 'bikeName' => 'Cola Bottle', 'bikeClass' => 'Road', 'suspensionType' => 'None', 'idealSurface' => 'Road', 'condition' => 'Excellent', 'brand' => 'Cannondale', 'model' => 'CAAD12', 'colour' => 'Black, silver and blue'] ];
-        $expectedOutput = '<div class="bikeCard"><h2>Name:</h2><p>'. 'Cola Bottle'. '</p><h3>Brand and model:</h3><p>' . 'Cannondale' . ' ' . 'CAAD12' . '</p><h3>Bike Type:</h3><p>' . 'Road' . '</p><h3>Condition:</h3><p>' . 'Excellent' . '</p><h3>Colour:</h3><p>' . 'Black, silver and blue' . '</p></div>';
+        $expectedOutput = '<div class="bikeCard">' . '<h2>' . 'Cola Bottle'. '</h2>' . '<h3>Brand and model:</h3><p>' . 'Cannondale' . ' ' . 'CAAD12' . '</p><h3>Bike Type:</h3><p>' . 'Road' . '</p><h3>Condition:</h3><p>' . 'Excellent' . '</p><h3>Colour:</h3><p>' . 'Black, silver and blue' . '</p></div>';
         $actualOutput = bikeDetails($input);
         $this->assertEquals($expectedOutput, $actualOutput);
     }
